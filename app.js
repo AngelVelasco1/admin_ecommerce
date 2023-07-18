f2906371e50141d70828f93b2c09b98e9c4d9742
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-07-17 19:07:32 -0500

    feat: :construction: Basic configuration.
    Implemented env, routes and the main file.

---
+++ app.js
@@ -0,0 +1,25 @@
+//? Dependencies
+import express from 'express';
+import dotenv from 'dotenv';
+
+//? Routes
+import storageProducts from './routes/products.js';
+import storageSuppliers from './routes/suppliers.js';
+import storagePromotions from './routes/promotions.js'
+
+//? Enviroment Variables
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+//? Use routes
+app.use("/products", storageProducts);
+app.use("/suppliers", storageSuppliers);
+app.use("/promotions", storagePromotions);
+
+//? Server
+const config = JSON.parse(process.env.MY_CONFIG);
+app.listen(config, () => {
+    console.log(`http://${config.hostname}:${config.port}`);
+})
