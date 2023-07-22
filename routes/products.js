a35d83703db635d66b0e8d9d4780453d7da1b812
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-07-21 22:20:00 -0500

    feat: :sparkles: Update products
    Update products by its id

--- routes/products.js
+++ routes/products.js
@@ -9,26 +9,20 @@ dotenv.config("../");
 const storageProducts = Router();
 
 //? Add products
-
 storageProducts.post('/add', proxyProducts, (req, res) => {
+    const { name, description, price, stock, discount_percentage } = req.body;
+    const productData = {
+        name,
+        description,
+        price,
+        stock,
+        discount_percentage
 
+    };
     try {
-
         const action = `INSERT INTO products SET ?`;
-        const { name, description, price, stock, discount_percentage, category } = req.body;
-
-        const productData = {
-            name,
-            description,
-            price,
-            stock,
-            discount_percentage,
-            category
-        };
-
-        conx.query(
+         conx.query(
             action, productData, (err) => {
-
                 if (err) {
                     console.error("Error de conexion:", err.message);
                     return res.status(500);
@@ -36,20 +30,14 @@ storageProducts.post('/add', proxyProducts, (req, res) => {
 
                 else {
                     return res.status(201).send(JSON.stringify(productData));
-
                 }
-
             });
-
     } catch (err) {
         res.status(500).send(err.message);
-
     }
-
 })
 
 //? List Products Order alphabetically
-
 storageProducts.get('/list', proxyProducts, (req, res) => {
 
     try {
@@ -72,13 +60,40 @@ storageProducts.get('/list', proxyProducts, (req, res) => {
 
 })
 
-
 //? Update Products
+storageProducts.patch('/update/:id', proxyProducts, (req, res) => {
+    const { id } = req.params;
+    const {name, description, price, stock, discount_percentage } = req.body;
+    const productData = {
+        name,
+        description,
+        price,
+        stock,
+        discount_percentage,
+    };
+    try {
+        const action = 'UPDATE products SET ? WHERE id = ?';
+        conx.query(action, [productData, id], (err, result)  => {
+            if (err) {
+                console.error("Error de conexion:", err.message);
+                return res.status(500);
+            } else {
+                return res.status(201).send(JSON.stringify(productData));
+            }
+        })
 
+
+    } catch (err) {
+        res.status(500).send(err.message);
+
+    }
+})
 
 //? Delete products
 
+
 //? List products related to specific category
+
 
 
 export default storageProducts;
