93932b102351694e9d0b30653308da152b2e4543
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-07-23 12:50:44 -0500

    feat: :sparkles: Register and login usign JWT
    Usign jwt, validations with DTO and more

--- auth/customerAuth.js
+++ auth/customerAuth.js
@@ -1,13 +1,25 @@
 //? Dependencies
 import express from 'express';
-import { SignJWT, jwtVerify } from 'jose';
-import { conx } from '../middlewares/proxyCustomer.js';
-import mysql2 from 'mysql2/promise';
+import { jwtVerify } from 'jose';
+import dotenv from 'dotenv';
+
+dotenv.config("../");
 
 let registerAuth = express();
 
-registerAuth.use((req, res, next ) => {
-    
-    next();
+registerAuth.use(async(req, res, next ) => {
+    const token = req.headers.authorization;
+
+    if(!token) return res.status(401).send('Not token found');
+
+    try {
+        const encoder = new TextEncoder();
+        const jwtData = await jwtVerify(token, encoder.encode(process.env.PRIVATE_KEY))
+        res.send(jwtData);
+        next()
+    } catch(err) {
+        console.error('Error de autenticación:', err.message);
+        return res.status(401).send('Token inválido');
+    }
 })
 export default registerAuth;
--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,9 @@
 //? Dependencies
 import { Router } from 'express';
 import dotenv from 'dotenv';
+import registerAuth from '../auth/customerAuth.js';
 import proxyCustomer from '../middlewares/proxyCustomer.js';
-import auth from '../auth/customerAuth.js';
+import { SignJWT } from 'jose';
 import { conx } from '../middlewares/proxyCustomer.js';
 
 //? Enviroment Variables
@@ -20,18 +21,26 @@ storageCustomer.post('/create', proxyCustomer, async(req, res) => {
         role_id: 2
     }
     try {
-        const checkCustomer = 'SELECT * FROM customer WHERE name = ?';
+        const checkCustomer = 'SELECT * FROM customer WHERE name = ? OR email = ?';
        
-        const [customerResult] = await conx.query(checkCustomer, [name]);
+        const [customerResult] = await conx.query(checkCustomer, [name, email]);
         if (customerResult.length > 0) {
           return res.status(409).send("El cliente ya existe");
         }
-      
-      
+    
         const action = 'INSERT INTO customer SET ?';
         await conx.query(action, userData);
-      
-        return res.status(201).send(userData);  
+        const encoder = new TextEncoder()
+        const jwtContruct = new SignJWT({userData})
+
+        const jwt = await jwtContruct
+        .setProtectedHeader({alg: "HS256", typ: "JWT"})
+        .setIssuedAt()
+        .setExpirationTime("30m")
+        .sign(encoder.encode(process.env.PRIVATE_KEY))
+
+        // Enviar el token en la respuesta
+        return res.status(201).json({ userData, jwt });
     } catch(err) {
         console.error('Error de conexion:', err.message);
         res.sendStatus(500);
@@ -39,8 +48,25 @@ storageCustomer.post('/create', proxyCustomer, async(req, res) => {
 })
 
 //? Login with jwt
-storageCustomer.post('/login', proxyCustomer, auth, (req, res) => {
+storageCustomer.get('/login', proxyCustomer, registerAuth, async(req, res) => {
+    const {name, email} = req.body;
+    const userData = {
+        name,
+        email
+    }
+    try {
+        if(!name || !email) return res.status(404).send("Envia tus datos");
 
+        const action = 'SELECT * FROM customer WHERE name = ? AND email = ?';
+        cons [user] = await conx.query(action, [userData]);
+
+        if(user.length == 0 ) return res.status(404).send("Usuario no encontrado")
+
+    } catch(err) {
+        console.error('Error de conexion:', err.message);
+         res.sendStatus(500);
+   
+    }
 })
 
 //? Delete account by id
